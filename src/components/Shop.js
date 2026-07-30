1abd1cc84c8bcc312edbf2d41efeca7048af9a20
Junaid <mdjunaidshaikhindore@gmail.com>
2026-07-30 22:16:16 +0530

    Just added some basic structure
    Nothing used.
    Installed : redux react-redux redux-thunk

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h4>Deposit / Withdraw money</h4>
+        <button className="btn btn-primary mx-2">-</button>
+        Update Balance
+        <button className="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
